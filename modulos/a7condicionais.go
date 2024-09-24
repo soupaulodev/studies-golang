995de2953a311411d1135f8fe34dfa3c4f31d554
Paulo Marques <paulomarques.rs1@gmail.com>
2024-09-24 10:57:13 -0300

    Add switch conditional

--- modulos/a7condicionais.go
+++ modulos/a7condicionais.go
@@ -5,12 +5,25 @@ import "fmt"
 // Condicionais no GO
 func Condicionais() {
 
+	// if, else if e else
 	for i := 0; i < 10; i++ {
 		if i == 5 {
 			fmt.Println("i == 5")
 		} else if i == 6 {
 			fmt.Println("i == 6")
 		} else {
+			fmt.Println("i != 5 && i != 6")
+		}
+	}
+
+	// switch
+	for i := 0; i < 10; i++ {
+		switch i {
+		case 5:
+			fmt.Println("i == 5")
+		case 6:
+			fmt.Println("i == 6")
+		default:
 			fmt.Println("i != 5 && i != 6")
 		}
 	}
