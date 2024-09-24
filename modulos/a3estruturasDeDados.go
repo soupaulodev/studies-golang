c230aaf8dbd4e65bde3d5e0bc53494fc891a588a
Paulo Marques <paulomarques.rs1@gmail.com>
2024-09-24 11:07:00 -0300

    Add Array Multidimensional

--- modulos/a3estruturasDeDados.go
+++ modulos/a3estruturasDeDados.go
@@ -13,6 +13,14 @@ func EstruturasDeDados() {
 	slice := []int{1, 2, 3, 4, 5}
 	slice = append(slice, 6)
 
+	// Array Multidimensional
+	matrix := [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	fmt.Println(matrix[0][0])
+
 	// Map
 	mapa := make(map[string]int)
 	mapa["key"] = 1
